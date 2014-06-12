24c7522fe824e32d15ee3256a2d34a282ac71d04
Drew Martin <drew.e.martin@gmail.com>
2014-06-12 19:41:57 -0400

    done 4 tday

--- crm.rb
+++ crm.rb
@@ -65,17 +65,17 @@ put '/contacts/:id' do
 	end
 end
 
-delete '/contacts/:id' do
-	@contact = @@rolodex.find(params[:id])
-
-	if @contact
-		@@rolodex.remove_contact(contact)
-		redirect '/contacts'
-	else
-		raise Sinatra::Notfound
 
 
-
+delete "/contacts/:id" do
+  @contact = @@rolodex.find(params[:id].to_i)
+  if @contact
+    @@rolodex.remove_contact(@contact)
+    redirect to("/contacts")
+  else
+    raise Sinatra::NotFound
+  end
+end
 
 
 
